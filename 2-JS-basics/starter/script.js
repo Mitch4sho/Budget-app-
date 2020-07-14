48029b1e14e32938e10edd00ef116f47d0866c32
iamolivermn <mitchelloliver@gmail.com>
2020-07-14 06:04:59 -0700

    Learned about variable mutation and type coercion

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -4,3 +4,33 @@ console.log(fullAge);
 
 var age = 28;
 console.log(age);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+
+// variable mutation and type coercion 
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is married? ' + isMarried);
+
+// variable  mutation
+
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is married? ' + isMarried);
+
+
+var lastName = prompt('whit is his last name');
+alert(firstName + ' ' + lastName);
